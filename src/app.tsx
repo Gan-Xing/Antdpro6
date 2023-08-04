939a85c060f732c080deab89d74ae50d407d46b5
GanXing <599153574@qq.com>
2023-08-04 18:11:17 +0800

    fix(test): test

--- src/app.tsx
+++ src/app.tsx
@@ -113,7 +113,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
       ? [
           <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
             <LinkOutlined />
-            <span>OpenAPI 文档</span>
+            <span>OpenAPI文档</span>
           </Link>,
         ]
       : [],
