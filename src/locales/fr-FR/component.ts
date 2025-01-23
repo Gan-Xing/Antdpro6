2135a460a024fa67584d4ebf51b6cf7e2a0bb439
ganxing <599153574@qq.com>
2025-01-23 23:59:09 +0000

    fix: 新增国际化

--- src/locales/fr-FR/component.ts
+++ src/locales/fr-FR/component.ts
@@ -2,4 +2,7 @@ export default {
   'component.tagSelect.expand': 'Développer',
   'component.tagSelect.collapse': 'Réduire',
   'component.tagSelect.all': 'Tout',
+  'component.avatar.personalCenter': 'Centre Personnel',
+  'component.avatar.personalSettings': 'Paramètres Personnels',
+  'component.avatar.logout': 'Se Déconnecter',
 };
--- src/locales/zh-CN/component.ts
+++ src/locales/zh-CN/component.ts
@@ -2,4 +2,7 @@ export default {
   'component.tagSelect.expand': '展开',
   'component.tagSelect.collapse': '收起',
   'component.tagSelect.all': '全部',
+  'component.avatar.personalCenter': '个人中心',
+  'component.avatar.personalSettings': '个人设置',
+  'component.avatar.logout': '退出登录',
 };
